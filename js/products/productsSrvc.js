4096f201e408cbae10d328924e5fda5a2ea4d90f
Scott Serage <xttocsurge51@gmail.com>
2017-10-31 17:10:25 -0600

    completed step 3

--- js/products/productsSrvc.js
+++ js/products/productsSrvc.js
@@ -1,3 +1,4 @@
-angular.module('myApp').service('productSrvc', function() {
-
+angular.module('myApp').service('productSrvc', function($http) {
+  this.getShoeData = () => {method: 'GET', url: 'https://practiceapi.devmountain.com/products?category=shoes'};
+  this.getSockData = () => {method: 'GET', url: 'https://practiceapi.devmountain.com/products?category=socks'};
 });
