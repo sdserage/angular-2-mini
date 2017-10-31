91626f56b66297734513df30ea716c74e2c8e24a
Scott Serage <xttocsurge51@gmail.com>
2017-10-31 16:36:59 -0600

    completed step 2

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
-angular.module('myApp', []);
+angular.module('myApp', ['ui.router']).config(function($stateProvider, $urlRouterProvider){
+  $stateProvider
+    .state(
+      'home', {
+        url: '/',
+        templateUrl: 'js/home/homeTmpl.html'
+    })
+    .state(
+      'products', {
+        url: '/products',
+        templateUrl: 'js/home/homeTmpl.html',
+        controller: 'productsCtrl'
+    })
+    .state(
+      'settings', {
+        url: '/settings',
+        templateUrl: 'js/settings/settingsTmpl.html'
+    });
+  $urlRouterProvider
+    .otherwise('/');
+});
